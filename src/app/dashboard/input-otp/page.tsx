0126eed5aa5a25db42291ae1301516c3ade76f68
Hugo <hugo@boahstudio.com>
2024-06-24 22:09:52 -0300

    chore: Update npm dependencies to include input-otp package

---
+++ src/app/dashboard/input-otp/page.tsx
@@ -0,0 +1,43 @@
+'use client'
+
+import {
+  InputOTP,
+  InputOTPGroup,
+  InputOTPSeparator,
+  InputOTPSlot,
+} from "@/components/ui/input-otp"
+import { useState } from "react";
+
+export default function Page() {
+
+  const [value, setValue] = useState("")
+
+  return (
+    <div className="flex flex-col justify-center items-center h-[250px]">
+      <InputOTP 
+        maxLength={6}
+        value={value}
+        onChange={(value) => setValue(value)}
+      >
+        <InputOTPGroup>
+          <InputOTPSlot index={0} />
+          <InputOTPSlot index={1} />
+          <InputOTPSlot index={2} />
+        </InputOTPGroup>
+        <InputOTPSeparator />
+        <InputOTPGroup>
+          <InputOTPSlot index={3} />
+          <InputOTPSlot index={4} />
+          <InputOTPSlot index={5} />
+        </InputOTPGroup>
+      </InputOTP>
+      <div className="text-center text-sm mt-3">
+        {value === "" ? (
+          <>Enter your one-time password.</>
+        ) : (
+          <>You entered: {value}</>
+        )}
+      </div>
+    </div>
+  );
+}
