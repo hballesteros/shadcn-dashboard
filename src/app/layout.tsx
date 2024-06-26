103fecdfb842da6b119dfb964fa9dc44212be9f7
Hugo <hugo@boahstudio.com>
2024-06-26 00:42:33 -0300

    feat: Add @radix-ui/react-toast package and update dashboard layout
    - Add @radix-ui/react-toast package to enable toast notifications in the application.
    - Update the dashboard layout by adding a new link for the "toast" page.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter as FontSans } from "next/font/google"
 import "./globals.css";
 import { cn } from "@/lib/utils";
 import { Toaster as SonnerToaster } from "@/components/ui/sonner"
+import { Toaster } from "@/components/ui/toaster"
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -26,6 +27,7 @@ export default function RootLayout({
           fontSans.variable
         )}>{children}
       <SonnerToaster richColors />
+      <Toaster />
       </body>
     </html>
   );
