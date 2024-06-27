f262de74cbe7783269329e90e2b2ac2f7e1f2290
Hugo <hugo@boahstudio.com>
2024-06-27 06:02:14 -0300

    feat: Add @radix-ui/react-dropdown-menu package and update data table columns
    - Add @radix-ui/react-dropdown-menu package to enable dropdown menu functionality in the application.
    - Update data table columns to include a new column for the client name.

--- src/app/dashboard/data-table/columns.tsx
+++ src/app/dashboard/data-table/columns.tsx
@@ -1,24 +1,81 @@
 'use client'
 
+import { Badge } from '@/components/ui/badge'
 import { Payment } from '@/data/payments.data'
 import { ColumnDef } from '@tanstack/react-table'
+import { MoreHorizontal } from "lucide-react"
+import { Button } from "@/components/ui/button"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
 
 
 export const columns: ColumnDef<Payment>[] = [
   {
+    accessorKey: "clientName",
+    header: "Client Name",
+  },
+  {
     accessorKey: "status",
     header: "Status",
+    cell: ({ row }) => {
+      const status = row.getValue("status") as string
+      const variant = (status === "pending") ? "secondary" 
+      : (status === "processing") ? "info"
+      : (status === "success") ? "success"
+      : (status === "failed") ? "destructive"
+      : "default"
+      return  <Badge variant={variant} capitalize >{status}</Badge> 
+    }
+  },
+  {
+    accessorKey: "amount",
+    header: () => <div className="text-right">Amount</div>,
+    cell: ({ row }) => {
+      const amount = parseFloat(row.getValue("amount"))
+      const formatted = new Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "USD",
+      }).format(amount)
+ 
+      return <div className="text-right font-medium">{formatted}</div>
+    },
   },
   {
     accessorKey: "email",
     header: "Email",
   },
   {
-    accessorKey: "amount",
-    header: "Amount",
-  },
-  {
-    accessorKey: "clientName",
-    header: "Client Name",
+    id: "actions",
+    cell: ({ row }) => {
+      const payment = row.original
+ 
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="h-8 w-8 p-0">
+              <span className="sr-only">Open menu</span>
+              <MoreHorizontal className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+            <DropdownMenuItem
+              onClick={() => navigator.clipboard.writeText(payment.id)}
+            >
+              Copy payment ID
+            </DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>View customer</DropdownMenuItem>
+            <DropdownMenuItem>View payment details</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      )
+    },
   },
 ]
