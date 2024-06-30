90e1e567d84a59bbe5a43ee9ddb66be7e331694c
Hugo <hugo@boahstudio.com>
2024-06-30 11:31:20 -0300

    feat: Update data table columns and add @radix-ui/react-dropdown-menu package
    - Update data table columns to include a new column for the client name.
    - Add @radix-ui/react-dropdown-menu package to enable dropdown menu functionality in the application.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google"
+import { ThemeProvider } from "@/components/ui/theme-provider"
+
 import "./globals.css";
 import { cn } from "@/lib/utils";
 import { Toaster as SonnerToaster } from "@/components/ui/sonner"
@@ -21,13 +23,21 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className={cn(
           "min-h-screen bg-background font-sans antialiased",
           fontSans.variable
-        )}>{children}
-      <SonnerToaster richColors />
-      <Toaster />
+        )}>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+          <SonnerToaster richColors />
+          <Toaster />
       </body>
     </html>
   );
